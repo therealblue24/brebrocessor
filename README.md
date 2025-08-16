f24ce34b12185c2d3d409c0585e0ec0620b67a40
therealblue24 <99767624+therealblue24@users.noreply.github.com>
2025-08-15 20:38:15 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# 🅱️re🅱️rocessor
+
+It re🅱️laces 🅱️s and 🅱️ (P) s with 🅱️
+
+Enter in🅱️ut via stdin and the 🅱️re🅱️rocessor will out🅱️ut the 🅱️re🅱️rocessed text in stdout
+
+## Build
+
+```sh
+$ ./build.sh
+```
+
+## Exam🅱️le usage
+
+```sh
+$ cat text.txt | ./🅱️re🅱️rocessor
+```
---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <ctype.h>
+
+static inline int replace(wchar_t c)
+{
+	int l = tolower(c);
+	return l == 'b' || l == 'p';
+}
+
+static inline void preprocess_one(wchar_t c)
+{
+	if(replace(c)) {
+		fprintf(stdout, "🅱️");
+	} else {
+		fputwc(c, stdout);
+	}
+	return;
+}
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	wchar_t c = WEOF;
+	while((c = fgetwc(stdin)) != WEOF) {
+		preprocess_one(c);
+	}
+	return 0;
+}
